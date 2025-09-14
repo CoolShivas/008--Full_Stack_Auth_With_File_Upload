40d3ef93a48dceb950c58f486ba61948eb38fd24
CoolShivas <shivas2710cool00@gmail.com>
2025-09-14 18:57:01 +0530

    SettingUp the packageJSON, gitIgnore and formation of server port.

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-console.log(
-  "Starting of Full Stack Authentication With File Upload using ExpressJS"
-);
+import express from "express";
+
+const server = express();
+
+const PORT = 7000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running at Port :-) ${PORT}`);
+});
